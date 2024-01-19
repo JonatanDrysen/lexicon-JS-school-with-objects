615b1be0f76df86da643256d33521a5f3dfeadd9
JonatanDrysen <jojodrysen@gmail.com>
2024-01-19 14:44:50 +0100

    add subject objects

---
+++ src/models/Subjects.js
@@ -0,0 +1,17 @@
+let mathematics = {
+    name: "Mathematics",
+    students: [],
+    teacher: {}
+}
+
+let programming = {
+    name: "Programming",
+    students: [],
+    teacher: {}
+}
+
+let english = {
+    name: "English",
+    students: [],
+    teacher: {}
+}
