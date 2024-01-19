f1cb671bc86490dd93f279571935c3b75ad6e67e
JonatanDrysen <jojodrysen@gmail.com>
2024-01-19 15:03:24 +0100

    add student objects

---
+++ src/models/Students.js
@@ -0,0 +1,34 @@
+let jonatanDrysen = {
+    name: "Jonatan Drysén",
+    age: "21",
+    gender: "male",
+    subjects: []
+}
+
+let johnDoe = {
+    name: "John Doe",
+    age: "30",
+    gender: "male",
+    subjects: []
+}
+
+let janeDoe = {
+    name: "Jane Doe",
+    age: "27",
+    gender: "female",
+    subjects: []
+}
+
+let alexJohnson = {
+    name: "Alex Johnson",
+    age: "12",
+    gender: "male",
+    subjects: []
+}
+
+let emilyMiller = {
+    name: "emilyMiller",
+    age: "25",
+    gender: "female",
+    subjects: []
+}
