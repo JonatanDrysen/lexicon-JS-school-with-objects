d4b49250f32a8dda46807911f19279e821fdd7f0
JonatanDrysen <jojodrysen@gmail.com>
2024-01-20 11:15:02 +0100

    add teacher to subject

--- src/index.js
+++ src/index.js
@@ -16,3 +16,20 @@ import {
     benjaminHughes 
 } from "./models/Teachers.js"
 
+// task 5
+katherineAnderson.subjects.push(mathematics)
+mathematics.teacher = katherineAnderson.name
+console.log("task 5: ", katherineAnderson)
+/*
+    I used push() instead of unshift() since there's no reason to add from the 
+    bottom of the array here.
+    This data is useful from an admins perspective because a school is a large
+    and complicated system which needs clear documentation to function efficiently.
+    It's useful for keeping track of what teacher is in charge of what class etc,
+    but this data structure as of now is very primitive and would not work at an
+    actual school since it would need to be scaled up immensely to work in an actual
+    school system. It would need alot more properties to keep track of, like timetables,
+    contact info, payment info, etc. just to name a few. The system would also need an
+    actual database to store the data, and the system would need to be alot more robust.
+*/
+
